e6d0782e68058fe71e0387e65c506685b7d414fa
Nestor04 <41174490+Nestor04@users.noreply.github.com>
2024-11-20 00:46:57 -0400

    Add files via upload

---
+++ procesar.php
@@ -0,0 +1,30 @@
+<?php
+// Conexión a la base de datos
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "registro";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Obtener datos del formulario
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+
+// Insertar datos en la base de datos
+$sql = "INSERT INTO usuarios (nombre, email) VALUES ('$nombre', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Registro exitoso";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Cerrar conexión
+$conn->close();
+?>
